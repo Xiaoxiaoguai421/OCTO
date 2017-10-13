1e82768ba2b50a2da31eb5531a112b3d72960bc7
Guangze <194463810@qq.com>
2017-10-13 18:07:23 +0800

    segh

---
+++ dev/2.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: 2.cpp
+	> Author: Guangze Zhu 
+	> Mail: xiaoxiaoguai421@gmail.com 
+	> Created Time: 2017年10月13日 星期五 18时07分10秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+sgrs
