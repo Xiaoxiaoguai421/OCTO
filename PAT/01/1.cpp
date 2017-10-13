7573a28aae2390d81c107ccf574d22eef2d79fe5
Guangze <194463810@qq.com>
2017-10-13 18:29:36 +0800

    nothing

---
+++ PAT/01/1.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: Guangze Zhu 
+	> Mail: xiaoxiaoguai421@gmail.com 
+	> Created Time: 2017年10月13日 星期五 18时28分20秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
