9f4dfa94799046c638e01bdab15f71f8d2e23d84
Guangze <194463810@qq.com>
2018-05-05 17:54:25 +0800

    debug matrix_chain

--- Algorithm/test/0410/matrix_chain.cpp
+++ Algorithm/test/0410/matrix_chain.cpp
@@ -5,52 +5,72 @@
     > Created Time: 2018年04月24日 星期二 15时04分51秒
  ************************************************************************/
 
-#include<iostream>
-#include<vector>
-#include<time.h>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 using namespace std;
 const int INFINITY = 99999;
-int matrix_chain(vector<int> p);
+void matrix_chain(vector<int> p);
+void print_matrix(int i, int j, vector<vector<int> > s);
 int main(void)
 {
-	int num = 0;
-	cout << "请输入矩阵数量：";
-	cin >> num;
-	vector<int> p(num + 1);
-	cout << "请输入"<<num+1<<"个矩阵信息：";
-	for (int i = 0; i <= num; i++)
-		cin >> p[i];
-	cout << "相乘的最少次数为：" << matrix_chain(p) << endl;
-	return 0;
+    int num = 0;
+    cout << "请输入矩阵数量：";
+    cin >> num;
+    vector<int> p(num + 1);
+    cout << "请输入矩阵信息:";
+    for (int i = 0; i < num + 1; i++)
+        cin >> p[i];
+    /*
+    srand((unsigned)time(0));
+    for (int i = 0; i < num; i++)
+        p[i]=rand()%50+1;*/
+    matrix_chain(p);
+    return 0;
 }
 
-int matrix_chain(vector<int> p)
+void matrix_chain(vector<int> p)
 {
-	int n = p.size()-1;
-	vector<vector<int> > m(n + 1);
-	vector<vector<int> > s(n + 1);  //记录k值
-	for (int i = 1; i <= n; i++)
-	{
-		m[i].resize(n + 1);
-		s[i].resize(n + 1);
-	}
-	for (int i = 1; i <= n; i++)
-		m[i][i] = 0;
-	for (int len = 2; len <= n; len++)
-		for (int i = 1; i <= n - len+1; i++)
-		{
-			int j = i + len-1;
-			m[i][j]= INFINITY;
-			for (int k = i; k < j; k++)
-			{
-				int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-				if (q < m[i][j])
-				{
-					m[i][j] = q;
-					s[i][j]=k;
-				}
-			}
-		}
-	return m[1][n];
+    int n = p.size();
+    vector<vector<int> > m(n);
+    vector<vector<int> > s(n); //记录k值
+    for (int i = 0; i < n; i++) {
+        m[i].resize(n);
+        s[i].resize(n);
+    }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            m[i][j] = 0;
+            s[i][j] = 0;
+        }
+
+    for (int len = 1; len <= n - 1; len++)
+        for (int i = 1; i <= n - 1 - len; i++) {
+            int j = i + len;
+            m[i][j] = INFINITY;
+            s[i][j] = i;
+            for (int k = i; k < j; k++) {
+                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                if (q < m[i][j]) {
+                    m[i][j] = q;
+                    s[i][j] = k;
+                }
+            }
+        }
+    cout << "相乘的最少次数为：" << m[1][n - 1] << endl;
+    print_matrix(1, n - 1, s);
+}
+
+void print_matrix(int i, int j, vector<vector<int> > s)
+{
+    if (i == j)
+        cout << "A" << i;
+    else {
+        cout << "(";
+        print_matrix(i, s[i][j], s);
+        print_matrix(s[i][j] + 1, j, s);
+        cout << ")";
+    }
 }
