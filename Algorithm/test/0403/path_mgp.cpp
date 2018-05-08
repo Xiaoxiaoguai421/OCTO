b57cfd1617c521cbdba0fc8a0eeb6064c64a5f1b
Guangze <194463810@qq.com>
2018-05-08 17:04:48 +0800

    debug print path

--- Algorithm/test/0403/path_mgp.cpp
+++ Algorithm/test/0403/path_mgp.cpp
@@ -7,9 +7,10 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 const int INFINITY = 99999;
-int shortest_path(vector<vector<int> > w); //起点到终点的最短路程
+void shortest_path(vector<vector<int> > w); //起点到终点的最短路程
 int main(void)
 {
     int n = 8;
@@ -27,16 +28,17 @@ int main(void)
     w[5][7] = 13;
     w[6][7] = 2;
 
-    cout << "最短路程: " << shortest_path(w) << endl;
+    shortest_path(w);
 
     return 0;
 }
 
-int shortest_path(vector<vector<int> > w)
+void shortest_path(vector<vector<int> > w)
 {
-    int n = w.size();
+    int n=w.size();
+    vector<int> prev(n);    //记录最短路径
     vector<int> d(n);
-    //    base case
+    //base case
     d[0] = 0;
     for (int i = 1; i < n; i++)
         d[i] = INFINITY;
@@ -45,10 +47,33 @@ int shortest_path(vector<vector<int> > w)
         int temp = d[i];
         for (int j = 0; j < i; j++) {
             if (w[j][i] != 0)
+            {
                 temp = d[j] + w[j][i];
-            if (temp < d[i])
-                d[i] = temp;
+                if (temp < d[i])
+                {
+                    d[i] = temp;
+                    prev[i]=j;
+                }
+            }
         }
     }
-    return d[n - 1];
+    prev.push_back(n-1);
+    cout<<"最短路程："<< d[n - 1] << endl;
+    cout<<"前驱节点: ";
+    for(int i=0;i<n;i++)
+        cout << prev[i] <<"";
+    cout<<endl;
+
+    int k=n-1;
+    vector<int> path;
+    while(prev[k]!=0)
+    {
+        path.push_back(k);
+        k=prev[k];
+    }
+    path.push_back(0);
+    cout<<"最短路径：";
+    for(int i=path.size()-1;i>0;i--)
+        cout<< path[i]  <<"->";
+    cout << path[0] << endl;
 }
